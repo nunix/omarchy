f194bd7110515edc2acf7d4da917df2d74345c5f
corsair <corsair@hey.com>
2025-09-09 10:50:00 +0200

    Added T2 specific configs

--- install.sh
+++ install.sh
@@ -46,6 +46,7 @@ source $OMARCHY_INSTALL/config/hardware/usb-autosuspend.sh
 source $OMARCHY_INSTALL/config/hardware/ignore-power-button.sh
 source $OMARCHY_INSTALL/config/hardware/nvidia.sh
 source $OMARCHY_INSTALL/config/hardware/fix-f13-amd-audio-input.sh
+source $OMARCHY_INSTALL/config/hardware/fix-suspend-t2.sh
 
 # Login
 source $OMARCHY_INSTALL/login/plymouth.sh

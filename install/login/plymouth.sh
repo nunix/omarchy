b9bc36ca93c36062118fcb4cf5218434516728d5
corsair <corsair@hey.com>
2025-09-10 21:17:24 +0200

    Updated plymouth script to remove linux-preset

--- install/login/plymouth.sh
+++ install/login/plymouth.sh
@@ -6,6 +6,10 @@
 # PLYMOUTH SETUP
 # ==============================================================================
 
+if [ -e "/etc/mkinitcpio.d/linux.preset" ]; then
+  sudo rm "/etc/mkinitcpio.d/linux.preset"
+fi
+
 if [ "$(plymouth-set-default-theme)" != "omarchy" ]; then
   sudo cp -r "$HOME/.local/share/omarchy/default/plymouth" /usr/share/plymouth/themes/omarchy/
   sudo plymouth-set-default-theme -R omarchy

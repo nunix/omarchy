e3a4a2dd9a75464207e988659de12b272888f8ca
corsair <corsair@hey.com>
2025-09-10 19:54:09 +0200

    Change the condition to set the keyboard backlight

--- install/config/hardware/fix-keyboard-backlight.sh
+++ install/config/hardware/fix-keyboard-backlight.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Set the keyboard backlight to maximum brightness on startup for MacBooks T2
-if command -v brightnessctl >/dev/null 2>&1; then
-  sudo brightnessctl --device ":white:kbd_backlight" set 100%
-else
+if [ -e "/sys/class/leds/:white:kbd_backlight/max_brightness" ]; then
   cat "/sys/class/leds/:white:kbd_backlight/max_brightness" | sudo tee "/sys/class/leds/:white:kbd_backlight/brightness"
 fi
